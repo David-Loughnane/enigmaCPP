060f7aab20bac1faff950bb6268ef69d3792c681
David Loughnane <dl4415@texel18.doc.ic.ac.uk>
2015-11-23 20:17:05 +0000

    commentary

--- enigma.cpp
+++ enigma.cpp
@@ -7,10 +7,8 @@ using namespace std;
 
 
 int main(int argc, char **argv) {
-  /*cout << "There are " << argc << " arguments:" << endl;
-  for (int nArg = 0; nArg < argc; nArg++)
-  cout << nArg << " " << argv[nArg] << endl;*/
 
+  //error if less than 3 command line arguments provided
   if (argc < 3) {
     cerr << "usage: enigma plugboard-file reflector-file (<rotor-file>* rotor-positions)?" << endl;
     return INSUFFICIENT_NUMBER_OF_PARAMETERS;
@@ -18,25 +16,27 @@ int main(int argc, char **argv) {
 
   int load_failure = 0;
 
+  //load Plugboard from 2nd cmd line arg
   Plugboard enigmaPlugboard;
   load_failure = enigmaPlugboard.load_input(argv[1]);
   if (load_failure) {
     return load_failure;
   }
 
+  //load Reflector from 3rd cmd line arg
   Reflector enigmaReflector;
   enigmaReflector.load_input(argv[2]);
   if (load_failure) {
     return load_failure;
   }
 
+  //define array of pointers to Rotors
   typedef Rotor** PtrRotorPtr;
   typedef Rotor* RotorPtr;
   PtrRotorPtr rotors_array;
   int rotors_index = 0;
 
-  //test to see if rotors in command line arguments
-  //bool set to true if no rotors are included
+  //if cmd line args < 3 then there are no Rotors
   bool no_rotors;
   if (argc > 3) {
     no_rotors = false;
@@ -45,6 +45,7 @@ int main(int argc, char **argv) {
     no_rotors = true;
   }
 
+  //call Rotor constructor for each Rotor cmd line arg
   if (!no_rotors) {
     rotors_array = new RotorPtr[argc - 4];
     for (int file_index = 3; file_index < (argc - 1); file_index++) {
@@ -53,6 +54,7 @@ int main(int argc, char **argv) {
       rotors_array[rotors_index] = rotor;
       rotors_index++;
     }
+    //laod Rotor configurations
     for (int i = 0; i < rotors_index; i++) {
       load_failure = rotors_array[i]->load_input(argv[3 + i], argv[argc - 1]);
       if (load_failure) {
@@ -62,68 +64,26 @@ int main(int argc, char **argv) {
     }
   }
 
-  /*TEST HEADER*/
-  /*cout << setw(6) << "CHAR" << setw(6) << "INT";
-  cout << setw(6) << "PLUG";
-
-  if (!no_rotors) {
-    for (int i = 0; i < rotors_index; i++) {
-      cout << setw(6) << "ROTR";
-    }
-  }
-
-  cout <<setw(6) << "RFLCT";
-
-  if (!no_rotors) {
-    for (int i = 0; i < rotors_index; i++) {
-      cout << setw(6) << "ROTR";
-    }
-  }  
-
-  cout << setw(6) << "PLUG";
-  cout << setw(6) << "CHAR" << endl;*/
-
-
   char input_char, output_char;
+  //input character, skip whitespace
   while (cin >> input_char) {
     if (input_char < 'A' || input_char > 'Z') {
       cerr << input_char << " is not a valid input character"
       << " (input characters must be upper case letters A-Z)!";
       return INVALID_INPUT_CHARACTER;
     }
-    //cout << setw(6) << input_char;
     int input_int = input_char - 65;
-    //cout << setw(6) << input_int;
 
-    /*for (int i = (rotors_index - 1); i >= 0; i--) {
-      cout << "rotor number " << i << endl;
-      for (int j = 0; j < rotors_array[i]->mapping_count; j++) {
-	cout << setw(3) << rotors_array[i]->relative_mapping[j] << " ";
-      }
-      cout << endl;
-    }
-    cout << endl;*/
-
+    //every time key is pressed, right hand rotor rotates, relative mapping updated
     if (!no_rotors) {
-    //cout << rotors_array[0]->relative_position << " ";
       rotors_array[rotors_index - 1]->relative_position++;
-    //cout << rotors_array[0]->relative_position << endl;
       rotors_array[rotors_index - 1]->set_relative_mapping();
     }
 
-    /*for (int i = (rotors_index - 1); i >= 0; i--) {
-    cout << "rotor number " << i << endl;
-    for (int j = 0; j < rotors_array[i]->mapping_count; j++) {
-      cout << setw(3) << rotors_array[i]->relative_mapping[j] << " ";
-    }
-    cout << endl;
-  }
-  cout << endl;*/
-
+  
   for (int i = (rotors_index - 1); i >= 0; i--) {
     for (int j = 0; j < rotors_array[i]->notch_count; j++) {
       if ((rotors_array[i]->relative_position % 26) == rotors_array[i]->notches[j]) {
-      //if (rotors_array[i]->relative_mapping[0] == rotors_array[i]->notches[j]) {
 	rotors_array[i-1]->relative_position++;
 	rotors_array[i-1]->set_relative_mapping();
       }
@@ -131,31 +91,25 @@ int main(int argc, char **argv) {
   }
 
   enigmaPlugboard.map_input(input_int);
-  //cout << setw(6) << input_int;
 
   if (!no_rotors) {
     for (int i = (rotors_index - 1); i >= 0; i--) {
       rotors_array[i]->map_input(input_int);
-      //cout << setw(6) << input_int;
     }
   }
 
   enigmaReflector.map_input(input_int);
-  //cout << setw(6) << input_int;
   
   if (!no_rotors) {
     for (int i = 0; i < rotors_index; i++) {
       rotors_array[i]->reverse_map_input(input_int);
-      //cout << setw(6) << input_int;
     }
   }
     
   enigmaPlugboard.map_input(input_int);
-  //cout << setw(6) << input_int;
-
-
+  
+  //final int value mapped to uppercase char
   output_char = input_int + 65;
-  //cout << setw(6) << output_char << endl;
   cout << output_char;
   }
 
